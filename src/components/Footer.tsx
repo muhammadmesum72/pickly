81d528b90cfc0b62a8fe7611300dafb9595a70e0
muhammadmesum72 <me.m.mesum@gmail.com>
2023-09-14 11:13:32 +0500

    Uploading

---
+++ src/components/Footer.tsx
@@ -0,0 +1,27 @@
+export function Footer() {
+  return (
+    <div className="w-full  py-12 bg-[#101010]">
+      <div className="text-lg text-center text-[20px] text-[#efffb5]">
+        <p className=" ">
+          $PICKLY is an entertainment project, intended solely for amusement
+          purposes, and not to be considered as an investment
+        </p>
+        <p className=" ">
+          opportunity. Unlike traditional investments, $PICKLY does not possess
+          intrinsic value or guarantee any financial returns. It is
+        </p>
+        <p className=" ">
+          crucial to conduct thorough research and exercise caution while
+          trading cryptocurrencies. Remember, the crypto market can
+        </p>
+        <p className=" ">
+          be volatile, so it’s essential to stay informed and make informed
+          decisions. Always do your research and approach crypto
+        </p>
+        <p className=" ">
+          trading with care.
+        </p>
+      </div>
+    </div>
+  );
+}
